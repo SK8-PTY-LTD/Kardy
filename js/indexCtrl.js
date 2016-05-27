775bb33d0ff072c89a4d6dd3438d39c03f8e9b04
JIANHAO_LI <kardy3415@hotmail.com>
2016-05-27 21:12:22 +1000

    personal homepage draft

---
+++ js/indexCtrl.js
@@ -0,0 +1,18 @@
+/**
+ * Created by JIANHAO_LI on 27/05/2016.
+ */
+angular.module("myWeb",[])
+.controller("indexCtrl", function ($scope) {
+
+    var status = true;                      //control the play of background music
+    $scope.musicControl = function () {
+        var music = angular.element("#music")[0];
+        if(status == true){
+            music.pause();
+        }else{
+            music.play();
+        }
+        status = !status;
+    }
+  
+})
